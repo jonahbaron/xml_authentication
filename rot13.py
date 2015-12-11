92cbc9c8bbeedd262338898f1869d501809bc5c2
Jonah Baron <jonahbaron0135@gmail.com>
2015-12-10 20:12:02 -0500

    Added file encryption/decryption functionality

--- rot13.py
+++ rot13.py
@@ -12,18 +12,40 @@ def rot13(text1):
 
 	return text2
 
+def rot13file(fname):
+	with open(fname) as f:
+		fcontent = f.readlines()
+
+	newcontent = []
+	for s in fcontent:
+		s = s.strip()
+		rot13line = rot13(s)
+		print rot13line
+		newcontent.append(rot13line)
+
+	with open("output.txt", "w") as f:
+		for s in newcontent:
+			f.write(s + "\n")
+
 def main():
 	parser = argparse.ArgumentParser(description='Encrypt or decrypt a string')
-	parser.add_argument("string", type=str, help="encrypt or decrypt entered string (ROT13 encryption = ROT13 decryption)")
+	parser.add_argument("-s", "--string", type=str, help="encrypt or decrypt an entered string (ROT13 encryption = ROT13 decryption)")
+	parser.add_argument("-f", "--filename", type=str, help="encrypt or decrypt an entered file and write output to output.txt")
 	args = parser.parse_args()
 
-	print "ROT13 Encryptor/Decryptor"
-	print ""
+	string = args.string
+	fname = args.filename
+	
 
-	print args.string
-	newstring = rot13(args.string)
-	print newstring
-
+	if args.filename is not None:
+		rot13file(fname)
+		
+	elif args.string is not None:
+		output = rot13(args.string)
+		print output
+	else:
+		print "ROT13 Encryptor/Decryptor"
+		print "Please run './rot13.py -h'"
 
 if __name__ == "__main__":
 	main()
